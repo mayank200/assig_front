94efa567136bfd7d21dbe2073ea97b390f23c9cc
mayank_jelly <mayank.sharma@akalinfosys.com>
2023-05-30 16:16:17 +0530

    changes

--- src/shared/helpers/constants.ts
+++ src/shared/helpers/constants.ts
@@ -4,6 +4,7 @@ import { environment } from '../../environments/environment';
 
 export const service_baseurl: string = environment.nodeApiUrlLatest;
 
+//  ng build --base-href=assig_front
 
 export const login_url: string = service_baseurl + "login";
 export const crud_operation_url: string = service_baseurl + "crud_operation";
