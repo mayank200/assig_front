ce3be347b1090cbdb3d2adb7cb9e28142d5fba40
mayank_jelly <mayank.sharma@akalinfosys.com>
2023-05-30 17:33:51 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { AuthGuard } from '../shared/guards/auth.guard';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: '**', pathMatch: 'full', redirectTo: 'login'},
   {
-    path: 'login',
+    path: 'assig_front/login',
     loadChildren: () =>
       import('./modules/login/login.module').then((m) => m.LoginModule),
   },
   {
-    path: 'products',
+    path: 'assig_front/products',
     canActivate:[AuthGuard],
     loadChildren: () =>
       import('./modules/products/products.module').then((m) => m.ProductsModule),
